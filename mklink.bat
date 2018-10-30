bbdc741a2ff82475ee75165c05213e34e7b96628
misonattou <misonattou@gmail.com>
2018-10-30 10:19:20 +0900

    Add vscode keybindings.json

--- mklink.bat
+++ mklink.bat
@@ -6,4 +6,7 @@ set DIR=%DIR:~0,-1%
 del /F /Q %APPDATA%\Code\User\settings.json
 mklink %APPDATA%\Code\User\settings.json %DIR%\vscode\settings.json
 
+del /F /Q %APPDATA%\Code\User\keybindings.json
+mklink %APPDATA%\Code\User\keybindings.json %DIR%\vscode\keybindings.json
+
 pause
